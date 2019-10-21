60b19b626ef2059016bf02db9aa5e6298af6e95e
Miguel <49990219+VonMisesHayek@users.noreply.github.com>
2019-10-21 15:31:08 -0300

    Update 999A30.c

--- 901-1000/999A30.c
+++ 901-1000/999A30.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+int main() {
+    int prob,skill;
+    scanf("%d %d",&prob,&skill);
+    int arr[prob];
+    int left=0;
+    int right=prob-1;
+    int count=0;
+    for (int i=0; i<prob; i++) {
+        int num;
+        scanf("%d",&num);
+        arr[i]=num;
+    }
+    while ((arr[left]<=skill || arr[right]<=skill) && count<prob) {
+        if (arr[left]<=skill) {
+            left++;
+        } else {
+            right--;
+        }
+        count++;
+    }
+    printf("%d",count);
+    return 0;
+}
